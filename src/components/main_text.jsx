9e91dcdcb806a956bee06e8f4dc0bca7ac60c14e
Vitaliy <mr.prikota@gmail.com>
2019-06-25 01:30:23 +0300

    Changed main window text

--- src/components/main_text.jsx
+++ src/components/main_text.jsx
@@ -7,7 +7,7 @@ function MainText() {
                 GitHub repositories viewer
             </div>
             <div id="text-main" className="animated">
-                That simple SPA app can be used to check your's or other user's GitHub repositories. All you need to do is just to fill username and click show repositories button.
+                That simple SPA can be used to check your's or other user's GitHub repositories. All you need to do is just to fill username and click show repositories button.
                 Don't worry if you made a mistake in username, app will just show you that it doesn't exist.
             </div>
         </div>
